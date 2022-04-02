ca60eca1c8f0c6d4dbccbda444a50d08ed54d938
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-04-01 23:27:14 -0300

    Add Gameplay Manager and Lose Condition

---
+++ Assets/Scripts/Gameplay/GameplayManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace NAMESPACENAME
+{
+    public class GameplayManager : MonoBehaviour
+    {
+        [Header("Set Values")]
+        [SerializeField] Transform pot;
+        [SerializeField] float loseHeight;
+
+        //Unity Events
+        private void Start()
+        {
+            if(pot == null)
+            {
+                pot = GameObject.FindGameObjectWithTag("Pot").transform;
+            }
+        }
+        private void Update()
+        {
+            CheckPotHeight();
+
+#if UNITY_EDITOR
+            DrawGameOverHeight(); 
+#endif
+        }
+
+        //Methods
+        const float lineRadius = 50;
+        void DrawGameOverHeight()
+        {
+            Vector2 lineStart = new Vector2(-lineRadius, loseHeight);
+            Vector2 lineEnd = new Vector2(lineRadius, loseHeight);
+            Debug.DrawLine(lineStart, lineEnd);
+        }
+        void CheckPotHeight()
+        {
+            if (pot.position.y < loseHeight)
+            {
+                Debug.Log("You lost");
+                Debug.Break();
+            }
+        }
+    }
+}
