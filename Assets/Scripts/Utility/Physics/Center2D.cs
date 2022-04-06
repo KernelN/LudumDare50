532723fd53a43beff69153dc82138e8f386500e9
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-04-06 16:44:40 -0300

    Add Center of Mass Modifier

---
+++ Assets/Scripts/Utility/Physics/Center2D.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Universal.CustomPhysics
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Center2D : CenterOfMassModifier
+    {
+        [Header("Set Values")]
+        [SerializeField] Rigidbody2D rBody;
+        [SerializeField] Vector2 center;
+
+        //Methods
+        internal override void DrawCenter()
+        {
+            Gizmos.DrawSphere((Vector2)transform.position + center, transform.lossyScale.x);
+        }
+        internal override void ModifyCenter()
+        {
+            if (rBody == null)
+            {
+                rBody = GetComponent<Rigidbody2D>();
+            }
+
+            rBody.centerOfMass = center;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Physics/CenterOfMassModifier.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Universal.CustomPhysics
+{
+    public abstract class CenterOfMassModifier : MonoBehaviour
+    {
+        //Unity Events
+        internal void Start()
+        {
+            ModifyCenter();
+        }
+#if UNITY_EDITOR
+        [ExecuteInEditMode]
+        internal void OnDrawGizmos()
+        {
+            DrawCenter();
+        }
+#endif
+
+        //Methods
+        internal abstract void DrawCenter();
+        internal abstract void ModifyCenter();
+    }
+}
